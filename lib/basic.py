755b00123a781a0f12fe7db4da19bd400a512a7b
Chris <csbroman10@gmail.com>
2021-09-01 23:44:31 +0100

    Added test and time commands.

--- lib/basic.py
+++ lib/basic.py
@@ -1,4 +1,5 @@
 import discord
+import time
 from discord.ext import commands
 
 
@@ -10,3 +11,18 @@ class Basic(commands.Cog):
     async def ping(self, ctx):
         ping = round(ctx.bot.latency * 1000)
         await ctx.send(f"My ping is {ping}ms.")
+
+    @commands.command(
+        name="test",
+        help="Checks if bot is operational."
+    )
+    async def test(self, ctx):
+        await ctx.send("Hello? Yes, I can hear you!")
+
+    @commands.command(
+        name="time",
+        help="Returns the time from AIO's region."
+    )
+    async def time(self, ctx):
+        local_time = time.localtime(time.time())
+        await ctx.send(f"It is currently {local_time[3]}:{local_time[4]} in my region (GMT+1).")
